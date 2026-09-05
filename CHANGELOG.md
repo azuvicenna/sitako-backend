026850b21a915189ba3449a1c152d2a47945be4c
Reeze <azfaalharits25@gmail.com>
2026-09-05 16:04:03 +0700

    feat: setup nodemailer for send email notification

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -21,3 +21,4 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 * Setup zod
 * Setup typst
 * Install xlxs (SheetJS)
+* Setup nodemailer
---
+++ src/utils/mail.ts
@@ -0,0 +1,26 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: Number(process.env.MAIL_PORT),
+  secure: true,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+export const sendEmail = async (to: string, subject: string, html: string) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `"SITAKO" <${process.env.MAIL_FROM}>`,
+      to,
+      subject,
+      html,
+    });
+    return { success: true, messageId: info.messageId };
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return { success: false, error };
+  }
+};
