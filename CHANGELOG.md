4ba26591a135bbe42e29ed3628ff86d93768872d
Reeze <azfaalharits25@gmail.com>
2026-09-05 14:38:08 +0700

    feat: setup drizzle-orm for postgresql and ULID for id generation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,3 +13,5 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 * Setup docker compose dan dockerfile
 * Setup .env
 * Setup jenkinsfile
+* Setup config for Cloudflare R2
+* Setup drizzle-orm with postgresql & ULID
---
+++ src/db/schema.ts
@@ -0,0 +1,204 @@
+import {
+  pgTable,
+  pgEnum,
+  boolean,
+  integer,
+  text,
+  uniqueIndex,
+  timestamp,
+} from "drizzle-orm/pg-core";
+import { generateId } from "../utils/ulid";
+
+export const bookTypeEnum = pgEnum("tipe_buku_enum", ["Fisik", "Digital"]);
+export const fineTypeEnum = pgEnum("tipe_denda_enum", ["Terlambat", "Hilang"]);
+export const calculationTypeEnum = pgEnum("tipe_kalkulasi_enum", [
+  "Akumulasi",
+  "Flat",
+]);
+export const transactionStatusEnum = pgEnum("status_transaksi_enum", [
+  "Menunggu Persetujuan",
+  "Dibatalkan",
+  "Menunggu Diambil",
+  "Dipinjam",
+  "Dikembalikan",
+  "Terlambat",
+  "Tidak Mengembalikan",
+]);
+export const paymentMethodEnum = pgEnum("metode_pembayaran_enum", [
+  "Tunai",
+  "Non-Tunai",
+]);
+
+export const librarians = pgTable("librarians", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => generateId()),
+  nama: text("nama").notNull(),
+  nip: text("nip").notNull(),
+  email: text("email").notNull(),
+  telepon: text("telepon").notNull(),
+  foto: text("foto").notNull(),
+  status_aktif: boolean("status_aktif").default(true),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export const members = pgTable("members", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => generateId()),
+  nama: text("nama").notNull(),
+  nis: text("nis").notNull(),
+  email: text("email").notNull(),
+  telepon: text("telepon").notNull(),
+  foto: text("foto").notNull(),
+  status_aktif: boolean("status_aktif").default(true),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export const shelves = pgTable("shelves", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => generateId()),
+  namaRak: text("nama_rak").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export const books = pgTable("books", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => generateId()),
+  judul: text("judul").notNull(),
+  penulis: text("penulis").notNull(),
+  isbn: text("isbn").notNull(),
+  penerbit: text("penerbit").notNull(),
+  genre: text("genre").array().notNull(),
+  tipeBuku: bookTypeEnum("tipe_buku").default("Fisik").notNull(),
+  tahunTerbit: integer("tahun_terbit").notNull(),
+  jumlahStok: integer("jumlah_stok").notNull(),
+  cover: text("cover").notNull(),
+  file: text("file_digital"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export const stacks = pgTable(
+  "stacks",
+  {
+    id: text("id")
+      .primaryKey()
+      .$defaultFn(() => generateId()),
+    rakId: text("rak_id")
+      .notNull()
+      .references(() => shelves.id, {
+        onDelete: "restrict",
+        onUpdate: "restrict",
+      }),
+    bukuId: text("buku_id")
+      .notNull()
+      .references(() => books.id, {
+        onDelete: "restrict",
+        onUpdate: "restrict",
+      }),
+    nomorSusunan: integer("nomor_susunan").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+  },
+  (table) => [uniqueIndex("rak_nomor_idx").on(table.rakId, table.nomorSusunan)],
+);
+
+export const bookmarks = pgTable("bookmarks", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => generateId()),
+  bukuId: text("buku_id")
+    .notNull()
+    .references(() => books.id, {
+      onDelete: "restrict",
+      onUpdate: "restrict",
+    }),
+  anggotaId: text("anggota_id")
+    .notNull()
+    .references(() => members.id, {
+      onDelete: "restrict",
+      onUpdate: "restrict",
+    }),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export const fines = pgTable("fines", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => generateId()),
+  bukuId: text("buku_id")
+    .notNull()
+    .references(() => books.id, {
+      onDelete: "restrict",
+      onUpdate: "restrict",
+    }),
+  jenisDenda: fineTypeEnum("jenis_denda").default("Terlambat").notNull(),
+  hargaDenda: integer("harga_denda").notNull(),
+  metodePerhitungan: calculationTypeEnum("metode_perhitungan")
+    .default("Akumulasi")
+    .notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export const transactions = pgTable("transactions", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => generateId()),
+  bukuId: text("buku_id")
+    .notNull()
+    .references(() => books.id, {
+      onDelete: "restrict",
+      onUpdate: "restrict",
+    }),
+  pustakawanId: text("pustakawan_id")
+    .notNull()
+    .references(() => librarians.id, {
+      onDelete: "restrict",
+      onUpdate: "restrict",
+    }),
+  anggotaId: text("anggota_id")
+    .notNull()
+    .references(() => members.id, {
+      onDelete: "restrict",
+      onUpdate: "restrict",
+    }),
+  kdTransaksi: text("kd_transaksi").notNull(),
+  tglPinjam: timestamp("tgl_pinjam").defaultNow().notNull(),
+  tglKembali: timestamp("tgl_kembali"),
+  status: transactionStatusEnum("status")
+    .default("Menunggu Persetujuan")
+    .notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export const finePayments = pgTable("payments", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => generateId()),
+  pustakawanId: text("pustakawan_id")
+    .notNull()
+    .references(() => librarians.id, {
+      onDelete: "restrict",
+      onUpdate: "restrict",
+    }),
+  anggotaId: text("anggota_id")
+    .notNull()
+    .references(() => members.id, {
+      onDelete: "restrict",
+      onUpdate: "restrict",
+    }),
+  transaksiId: text("transaksi_id")
+    .notNull()
+    .references(() => transactions.id, {
+      onDelete: "restrict",
+      onUpdate: "restrict",
+    }),
+  hargaDenda: integer("harga_denda").notNull(),
+  totalDenda: integer("total_denda").notNull(),
+  tglBayar: timestamp("tgl_bayar"),
+  metodePembayaran: paymentMethodEnum("metode_pembayaran")
+    .default("Tunai")
+    .notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
